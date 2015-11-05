6fe4a3e3e669d726917bbb24324908f401e038af
Kyle R <KyleRichVA@gmail.com>
2015-11-05 11:00:13 -0800

    Added program skeleton

---
+++ PyCipher.py
@@ -0,0 +1,32 @@
+'''
+A simple Python Program which takes text or text file and converts it to
+a 'sceret message' which can be converted back to a human readable text
+Created by Kyle Richardson
+'''
+
+import os
+import shelve
+import string
+
+# Set Character Lists for upper and lower case
+LOWER = string.ascii_lowercase
+UPPER = string.ascii_uppercase
+
+
+def make_secret(text=None, file=None):
+    """make_secret: Converts either text or file to a secret code file"""
+    pass
+
+
+def read_secret(file):
+    """read_secret: Takes a secret code file and converts it
+    to human readable text"""
+    pass
+
+
+def menu():
+    """Main Menu of the Program first thing that runs."""
+    pass
+
+if __name__ == "__main__":
+    menu()
