0bc2711b1ba8ee02e9735b6158d3ef53aa6c7ae3
Kyle R <KyleRichVA@gmail.com>
2015-11-07 09:37:27 -0800

    started on writesecret and stepdict functions

--- PyCipher.py
+++ PyCipher.py
@@ -7,6 +7,7 @@ Created by Kyle Richardson
 import os
 import shelve
 import string
+import random
 
 # Set Character Lists for upper and lower case
 LOWER = string.ascii_lowercase
@@ -15,7 +16,13 @@ UPPER = string.ascii_uppercase
 
 def make_secret(text=None, file=None):
     """make_secret: Converts either text or file to a secret code file"""
-    pass
+    # get a random value between 1 and 5 for the total steps.
+    step = random.randint(1, 5)
+    # make the step either negative or positive steps randomly
+    direction = random.choice([-1, 1])
+    step = step * direction
+    # create a dictionary which aligns each char to it's secret version.
+    cipher = stepdict(step)
 
 
 def read_secret(file):
@@ -23,6 +30,35 @@ def read_secret(file):
     to human readable text"""
     pass
 
+
+def stepdict(step):
+    """takes a step and creates a dictonary converting each char based off equation
+    """
+    index = 0
+    cipher = {}
+    # goes through all possible characters and converts them to a secret char
+    # based off the step.
+    for char in LOWER:
+        newindex = index + step
+        # loops around
+        if (newindex > 25):
+            newindex = newindex - 26
+        if (newindex < 0):
+            newindex = 26 + newindex
+        cipher[char] = LOWER[newindex]
+    for char in UPPER:
+        newindex = index + step
+        # loops around
+        if (newindex > 25):
+            newindex = newindex - 26
+        if (newindex < 0):
+            newindex = 26 + newindex
+        cipher[char] = UPPER[newindex]
+    return cipher
+
+
+def codedict(code):
+    """takes a code anf converts each char based off equation"""
 
 def menu():
     """Main Menu of the Program first thing that runs."""
